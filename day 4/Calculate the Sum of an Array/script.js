34ee5d8c85e511188f48552aa46256e44b50c02d
Mejba Hasan <mejbahassan135@gmail.com>
2026-08-12 23:59:21 +0600

    Calculate the Sum of an Array

---
+++ day 4/Calculate the Sum of an Array/script.js
@@ -0,0 +1,15 @@
+const arrayInput = document.getElementById("Array");
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", () => {
+    let arr ;
+    try {
+        arr = JSON.parse(arrayInput.value);
+    } catch (error) {
+       
+        return alert("Enter a valid array");
+    }
+    const sum=arr.reduce((acc,curr)=> acc+curr,0)
+   result.innerText=`Sum of the array is ${sum}`
+})
